290c0dfcfa239695af41d16ff6484ecc4e4c555c
zack149 <zpjordan18@gmail.com>
2021-10-30 14:59:22 -0500

    updated main

--- source/main.cpp
+++ source/main.cpp
@@ -1,1 +1,99 @@
-// empty for now
+#include "common.h"
+
+using namespace Angel;
+
+Frog frog;
+// we could declare the other objects here too
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+    // Key callbacks for hopping: might end up modifying slightly
+    if (key == GLFW_KEY_LEFT && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        frog.hopLeft();
+    if (key == GLFW_KEY_RIGHT && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        frog.hopRight();
+    if (key == GLFW_KEY_UP && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        frog.hopUp();
+    if (key == GLFW_KEY_DOWN && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        frog.hopDown();
+}
+
+//Calls update 5 times a second
+void animate()
+{
+    if(glfwGetTime() > 0.2){
+        glfwSetTime(0.0);
+        //frog.update_state();
+        //obstacles.update_states();
+    }
+}
+
+int main(void)
+{
+  GLFWwindow* window;
+  
+  glfwSetErrorCallback(error_callback);
+  
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
+  
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  
+  glfwWindowHint(GLFW_SAMPLES, 10);
+  
+  
+  // Change window size(?)
+  window = glfwCreateWindow(640, 480, "Frogger", NULL, NULL);
+  if (!window){
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+  
+  glfwSetKeyCallback(window, key_callback);
+  
+  glfwMakeContextCurrent(window);
+  gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+  glfwSwapInterval(1);
+  
+  init();
+  
+  while (!glfwWindowShouldClose(window)){
+    
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    
+    //Pick a coordinate system that makes the most sense to you
+    //(left, right, top, bottom)
+    mat4 proj = Ortho2D(-1.0, 1.0, -1.0, 1.0);
+    
+    animate();
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+
+     
+    //Draw all the sprites here 
+    //frog.draw(proj);
+    //everything_else.draw(proj);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+    
+  }
+  
+  glfwDestroyWindow(window);
+  
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
+}
